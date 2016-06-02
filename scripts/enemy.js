7ec9840f02fe156dc7a3998d1464c5262ed73d54
nvlaarhoven <nvlaarhoven@gmail.com>
2016-06-02 12:11:38 +0100

    Make sure GroundEnemy doesn't fall off cliff

--- scripts/enemy.js
+++ scripts/enemy.js
@@ -26,7 +26,26 @@ Quintus.ActionPlatformerEnemy = function(Q) {
     init: function(p) {
       this._super(p, {vx: -50, defaultDirection: "left"});
       this.add("2d, aiBounce, commonEnemy");
+    },
+    step: function(dt) {
+      var dirX = this.p.vx / Math.abs(this.p.vx);
+      var ground = Q.stage().locate(this.p.x, this.p.y + this.p.h/2 + 1, Q.SPRITE_DEFAULT);
+      var nextElement = Q.stage().locate(this.p.x + dirX * this.p.w/2 + dirX, this.p.y + this.p.h/2 + 1, Q.SPRITE_DEFAULT);
+      var nextTile;
+
+      if(nextElement instanceof Q.TileLayer) {
+        nextTile = true;
+      }
+
+      //if we are on ground and there is a cliff
+      if(ground && !nextTile) {
+        if ((this.p.vx > 0 && this.p.defaultDirection == "right") || (this.p.vx < 0 && this.p.defaultDirection == "left")) {
+            this.p.flip = "x";
+        } else {
+            this.p.flip = false;
+        }
+        this.p.vx = -this.p.vx;
+      }
     }
   });
-
 };
